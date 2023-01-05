66ab846c14eaad87ee624999324c8b65484d5675
mark raphael <mark.raphael@bt.com>
2023-01-05 12:56:34 +0000

    Minor refactor

--- live_classes/D_Operators/C_Object_Equality_Overriding_Equals.py
+++ live_classes/D_Operators/C_Object_Equality_Overriding_Equals.py
@@ -2,17 +2,27 @@ class House:
     def __init__(self, no_of_rooms):
         self.no_of_rooms = no_of_rooms
 
-    # This is an example of how to Override equals
+    """
+    Overriding equals is necessary as the default implementation of Python will consider
+    that two objects are different even if they have the same values. The only exception
+    to this rule is if you are utilising a String class which either has a single character
+    or multiple chars which equals another string either by reassignment or by 
+    declaration/initialisation which will return true.
+    This is an example of how to Override equals
+    """
     def __eq__(self, other):
-        if not isinstance(other, House):
-            return NotImplemented
-
+        if self.__class__ == other.__class__:
+            return True
+        if other is None:
+            return False
+        if self.__class__ != other.__class__:
+            return False
         return self.no_of_rooms == other.no_of_rooms
 
 
 if __name__ == '__main__':
     instance1 = House(5)
-    instance2 = House(4)
+    instance2 = House(5)
 
     if instance1 == instance2:
         print("Both instances are equal")

827ca0e5941db8720b14ba081a85a6a6b0fa38ed
mark raphael <mark.raphael@bt.com>
2023-01-16 13:47:42 +0000

    Minor refactor

--- live_classes/H_Exception_And_Error_Handling/A_Introduction.py
+++ live_classes/H_Exception_And_Error_Handling/A_Introduction.py
@@ -1,32 +1,31 @@
 """
 
-Today we will focus on the details of how to raise and handle exceptions which
-will assist to make our code more robust and to prevent potential failures that would
-otherwise cause a program to stop in an uncontrolled manner
+Raising and handling exceptions assists us to make our code more robust and to prevent
+potential failures that would otherwise cause a program to stop in an uncontrolled manner
 
 Exceptions are a powerful way to communicate unusual circumstances or error conditions
 without requiring a calling function to explicitly check return values
+---------------
 
 
+Errors are unrecoverable, Exceptions are routine:
+  -> Languages like Java build the distinction into the language
+  -> Languages like Python treat them as synonyms (there is a
+     subtle difference between an error and an exception)
 
-In Python, there is a subtle difference between an error and an exception
-
-An Error might indicate critical problems that a reasonable application should not try to catch,
+An Error might indicate critical problems that a reasonable application should NOT try to catch,
 while an Exception might indicate conditions that an application should try to catch at runtime
 
-Errors are a form of an unchecked exception and are irrecoverable like an OutOfMemoryError
-Even if the syntax of a statement or expression is correct, it may still cause an error when executed.
+Errors are a form of an unchecked exception and are irrecoverable. An example of one is MemoryError
 
-Python exceptions are types of errors that are detected during runtime and are not unconditionally fatal
+Python exceptions are types of errors that are detected during runtime and are not completely fatal
+Even if the syntax of a statement or expression is correct, it may still cause an error when executed
+-----------------
 
 
-
-An exception is an object
-
-We can define our own exceptions if we want to
-
-All exceptions inherit from a built-in class called BaseException
-
-When an exception occurs you generally read the stacktrace from the bottom-up
+- An exception is an object
+- In Python, all exceptions inherit from a built-in class called BaseException
+- We can define our own exceptions if we want to
+- When an exception occurs you generally read the stacktrace from the bottom-up
 
 """
--- live_classes/H_Exception_And_Error_Handling/B1_How_To_Cause_An_Exception_To_Occur.py
+++ live_classes/H_Exception_And_Error_Handling/B1_How_To_Cause_An_Exception_To_Occur.py
@@ -1,3 +1,12 @@
+"""
+Below are examples of exceptions. They can be handled at runtime and are not unconditionally
+fatal
+
+Refer to the Python Docs for more info:
+https://docs.python.org/3/library/exceptions.html?highlight=exceptions#exception-hierarchy
+"""
+
+
 def error_1():
     x = 5 / 0   # Raises ZeroDivisionByZero
 
@@ -12,14 +21,14 @@ def error_3():
     d['b']  # Raises KeyError 'b'
 
 
-def error_4():
-    # print(undef_variable)   # Raises NameError 'undef_variable' is not defined
+# def error_4():
+#     print(undef_variable)   # Raises NameError 'undef_variable' is not defined
 
 
 def main():
-    error_1()
+    # error_1()
     # error_2()
-    # error_3()
+    error_3()
     # error_4()
 
 
